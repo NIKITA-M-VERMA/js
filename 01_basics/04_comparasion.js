c57f9614f07df13d3fdd792c482bde761165f4d4
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 13:33:09 +0000

    compare

---
+++ 01_basics/04_comparasion.js
@@ -0,0 +1,9 @@
+console.log(null>=0)
+//return true
+//reason if == and comparator works differently 
+//meaning when == sign is used it gives false where as when =>is used it gives true as it converts the null to the 0 value 
+//undefined gives value false 
+
+//strict check 
+//===
+//datatype check and check the value also 
