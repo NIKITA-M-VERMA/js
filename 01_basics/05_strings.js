d97605e90fb066b428f43bc711aaa7d580064f06
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 14:37:45 +0000

    string proto

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+/*
+string concatinate + or ,
+*/
+
+/* use '' backticts this makes placeholders  in which you can place the variables
+string interpolation 
+ */
+
+//console.log(`my name is ${}and my age is ${}`);
+const gamename= new String('daisy')
+//String has some methods and the properties  and funstions ,its a object 
+//slice accets teh negative value substring do not do that 
+//trim removes the spaces  whitesaces and \n  trimend and trimstart 
+//replace search and replce the specific value 
+//split on the bases of criteria need seperator and limit 
