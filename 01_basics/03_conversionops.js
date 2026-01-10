75adebfd9114624373d6ca8100f8354af1b6ec66
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 13:23:08 +0000

    operations

--- 01_basics/03_conversionops.js
+++ 01_basics/03_conversionops.js
@@ -1,27 +1,42 @@
 let score ="55abd"
-console.log(typeof(score))
-console.log(typeof score )
+// console.log(typeof(score))
+// console.log(typeof score )
 
 let valueinNumber = Number(score)
 
-console.log(valueinNumber);
-console.log(typeof valueinNumber)
-console.log(typeof(valueinNumber))
+// console.log(valueinNumber);
+// console.log(typeof valueinNumber)
+// console.log(typeof(valueinNumber))
 /* null value gives 0
    undefined value gives nan
    not a number value gives nan
    type of nan is number
    */
- let islogin=" "
- let boollogin= Boolean(islogin)
- console.log(boollogin) 
+//  let islogin=" "
+//  let boollogin= Boolean(islogin)
+//  console.log(boollogin) 
  /* empty string gives false 
     filled string gives true
     0 gives false 
     1 gives true */
 
 
-let city= 22
-let strnum=String(city)
-console.log(typeof strnum)
-console.log(strnum)
+// let city= 22
+// let strnum=String(city)
+// console.log(typeof strnum)
+// console.log(strnum)
+
+
+//operations 
+
+let value="d"
+let negvalue=-value
+console.log(negvalue)
+
+/* empty string gets  -0
+space strings gets -0
+alphabet gets nan
+*/
+
+console.log(+true)
+console.log(true)
