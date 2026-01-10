2c64c8381b8cdf60712ecc1efb9a32cd0594a39f
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 12:26:16 +0000

    updated datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,13 @@
 "use strict"
 //treat all js code as newer version 
+//ECMA  documentataion
+
+/*number(2 power 53) 
+  string  
+  boolean 
+  null is a standalone value (it is a type of object)
+  undefined(value not defined)(it is undefined type)
+  symbol(for uniqqueness in react )
+  object 
+  */
+
