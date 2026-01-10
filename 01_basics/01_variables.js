7b128f2e889dc12bfed7d02b78aa5556bfee4c9d
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 07:51:19 +0000

    basics ack

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=145
+ //imuttable
+
+let email="nikitavjfb2gmail.com"
+//it helps with the scope 
+
+var name ="aseeha"
+// do not use var because it has the sccope problem and changes the value everywhere 
+//block scope and functional scope issue 
+//preferably do not use var 
+
+accountType="savings"
+let accountState
+//undefined as the value is not defined
+
+//accountId=156
+
+console.log(accountId);
+console.table([email,accountId,accountType, accountState])
