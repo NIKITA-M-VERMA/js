c63645abbdce0ec8d7ae70200b18904f61a7158c
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-10 12:45:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionops.js
@@ -0,0 +1,27 @@
+let score ="55abd"
+console.log(typeof(score))
+console.log(typeof score )
+
+let valueinNumber = Number(score)
+
+console.log(valueinNumber);
+console.log(typeof valueinNumber)
+console.log(typeof(valueinNumber))
+/* null value gives 0
+   undefined value gives nan
+   not a number value gives nan
+   type of nan is number
+   */
+ let islogin=" "
+ let boollogin= Boolean(islogin)
+ console.log(boollogin) 
+ /* empty string gives false 
+    filled string gives true
+    0 gives false 
+    1 gives true */
+
+
+let city= 22
+let strnum=String(city)
+console.log(typeof strnum)
+console.log(strnum)
