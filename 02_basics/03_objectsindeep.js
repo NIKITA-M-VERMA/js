5e368b31edc807b375cff7e615cd7438b0ea832c
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-11 09:01:25 +0000

    objects in detail

---
+++ 02_basics/03_objectsindeep.js
@@ -0,0 +1,18 @@
+/*
+objects using cntructor 
+singleton object 
+*/
+
+const zulipuser= new Object()//singleton object 
+const elementuser={}// non singleton object 
+
+
+zulipuser.id="1536"
+//you can use nested object and access them using . operator 
+//optional chaining 
+//object.assign({},obj1,obj2) all values goes in emplty array given 
+//if not provided all values goesin the object defined first in the assign method
+//easiest way of usign spread operator 
+//object.keys and object.values retuen the array 
+//entries makes array of each entry 
+//useownproperty used for inspection of the  properties it consist
