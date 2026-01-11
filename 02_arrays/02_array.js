e444d051c70fcc84d33cffca0d54ee1a1514af3c
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-11 07:48:37 +0000

    arrays

---
+++ 02_arrays/02_array.js
@@ -0,0 +1,28 @@
+const myarray=[1,2,"mihika",true]
+console.log(myarray)
+//mix of datatypes , and resizable 
+//array copy operations creates shallow copy(sharing same reference pont )
+
+const arr2= new Array(1,2,5 , 6 )
+//arrays have prototype and a properry of length 
+
+//methods
+/* push pop unshift(adds in front of array ) shift(delete from the front ) includes(return a boolean) index(return a index )
+ join(add all elements in s string the type changes )
+ slice(returns an sections of array range last  not include ) splice (include the last range )
+
+ splice manipulates the original array 
+ */
+
+
+
+ /* concatinate combines two array element by elemnet and returns a anew array 
+ push adds the other array as an element to existing array 
+ other easy way is usee spread 
+ ...heroes,...dc_hero
+ flat returns a new array where are subarray are concatinated in a anew array recursively upto certain depth d can be infinity also 
+ you can inspect an array 
+ convert an array 
+ incase of object specify the keys or value 
+ aray of  used to make aray from different vlue 
+ */ 
