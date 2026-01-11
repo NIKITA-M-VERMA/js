c25ec19885c59506c6380622ddaebca02f736175
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-11 06:47:01 +0000

    maths

---
+++ 01_basics/06_NumsAndMaths.js
@@ -0,0 +1,21 @@
+const score = new Number(100)
+
+console.log(score)
+
+//it has proto and properties also methods 
+//for more propeerties we can convert it to string and use proto and functions in string
+//use  fixed for precision 
+//for overall round off use precision
+
+
+
+/* maths is an object which has so many properties and function associated 
+
+*/
+
+console.log((Math.random()*10 )+1)
+//for random no. generated 
+
+const min =10
+const max=20
+console.log(Math.floor((Math.random()*(max-min+1)))+10)
