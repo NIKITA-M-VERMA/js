4abc3cdb90e401c3006cbdccba1ac3c26a0db462
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-26 04:58:53 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,8 @@
 let a =10
 const b=20
 var c=30
+
+console.log(a)
+//a and b are scopes specific 
+//c var is not scope specific var canbe accessed outside the scope therefore the var is not preferred 
+//global scope is available in local scope but local scope is not available in global scope //sccope in the inspect is different from scope declared in running of code using node 
