cdb8d6363a70452c2bbae64f214fcf9b40b90caf
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-01-26 05:19:31 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,3 +6,38 @@ console.log(a)
 //a and b are scopes specific 
 //c var is not scope specific var canbe accessed outside the scope therefore the var is not preferred 
 //global scope is available in local scope but local scope is not available in global scope //sccope in the inspect is different from scope declared in running of code using node 
+
+
+function one(){
+const user="hiketish"
+function two(){
+   const  website="gogo"
+    console.log(user)
+}
+console.log(user )
+two()
+}
+
+
+one()
+
+
+
+//function
+//if function is called before declaration no error is reported 
+
+function addone(num){
+    return num+1
+}
+
+addone(45)
+
+//variable
+//is variable in called before declaration error is reported 
+
+const addtwo= function(num){
+    return num+2
+}
+
+
+console.log(addtwo(45))
