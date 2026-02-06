a9265da08c8d82e30112c16330393c8e528b9bd9
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-02-06 07:02:04 +0000

    controlflow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,32 @@
+/*
+1. if(){
+}else{
+    }
+
+2. switch(){
+cases 
+break
+default
+}   
+
+3. truthy and fasy value
+falsy= false, 0,-0,bigint 0n, null, undefined , nan," "
+thruthy='false',' ',{},"0", function(){},[]
+
+4. false==0 gives true 
+   ""==0 gives true 
+
+5.Nullish coalescing operator (??)
+let val1;
+val1=5??10
+val1= null ?? 10
+it is basically null and undefined check 
+and gets first valid value 
+val1 null?? 10??20
+
+
+6. ternary operrator 
+condition  ? true ; false
+
+comparator <,>,<=,>=,==,!=,===(type check) ,&&,||
+*/
