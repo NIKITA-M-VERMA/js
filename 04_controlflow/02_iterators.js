99b31cea9f0aa3d617d64477f8d1bafff96bcca6
NIKITA M VERMA <128211570+NIKITA-M-VERMA@users.noreply.github.com>
2026-02-09 14:19:32 +0000

    iterators

--- 04_controlflow/02_iterators.js
+++ 04_controlflow/02_iterators.js
@@ -6,4 +6,26 @@ break (break the loop and come out )
 and 
 continue (overhead skip pass)
 
+
+2. while(){}
+
+3. do {}while ()
+execute atleast one even in worst case 
+
+
+4. for (const i of object){}
+
+
+5. maps- unique  we can use for of loop for( const [key,value] of map)
+
+6. objeccts is not iteratable using for of 
+
+
+7. for in is used to ieterate in the object different keys for(const key in obj)
+
+8. for in can also be used in array 
+
+9. for in is not iteratable for maps
+
+1 for each used in array -direct properties usin callback function and this function do not have its own name 
 */
